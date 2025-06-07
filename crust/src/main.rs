3129132dbc7ff3b3cb42994902dabefe9aabbc55
Binit Rupakheti <cruiserponder@protonmail.com>
2025-06-07 21:38:36 +0545

    use clap for the cli handling

--- crust/src/main.rs
+++ crust/src/main.rs
@@ -1,19 +1,39 @@
-use std::env;
-
+use clap::{Parser, Subcommand};
 use interpreter::crust::Crust;
 
+#[derive(Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+
+    #[arg(short, long, default_value = "interpreter")]
+    backend: Option<String>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Run { file_path: String },
+    Repl {},
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     let mut crust = Crust::new();
 
-    match env::args().nth(1).as_deref() {
-        Some("repl") => {
+    if let Some(backend) = cli.backend {
+        if backend != "interpreter" {
+            eprintln!("Unsupported backend: {}", backend);
+            return;
+        }
+    }
+
+    match cli.command {
+        Command::Run { file_path } => {
+            crust.run_file(&file_path);
+        }
+        Command::Repl {} => {
             crust.repl();
-        }
-        Some(path) => {
-            crust.run_file(path);
-        }
-        None => {
-            eprintln!("Usage: crust <script.crust> | crust repl");
         }
     }
 }
