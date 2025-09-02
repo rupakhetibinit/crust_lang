5c5991c950b58f65456d49ebda51f3ee29229938
Binit Rupakheti <cruiserponder@protonmail.com>
2025-09-02 10:48:00 +0545

    lexer spannedtoken for easy tracking of tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,6 @@
-use logos::Logos;
+use std::{fmt::format, ops::Range};
+
+use logos::{Logos, Span};
 
 #[derive(Logos, Debug, Clone)]
 pub enum Token {
@@ -119,20 +121,115 @@ pub enum Token {
 
     #[token(",")]
     Comma,
+
+    #[regex(r#""[^"]*""#, parse_string_slice)]
+    StringLiteral(String),
+
+    #[regex(r"//[^\n]*", |lex| lex.slice().to_string())]
+    LineComment(String),
+
+    #[regex(r"/\*[^*]*\*+(?:[^/*][^*]*\*+)*/", |lex| lex.slice().to_string())]
+    MultiLineComment(String),
+}
+
+fn parse_string_slice<'lex>(lex: &mut logos::Lexer<Token>) -> Option<String> {
+    let slice = lex.slice();
+    Some(slice[1..slice.len() - 1].to_string())
+}
+
+impl<'t> ToString for Token {
+    fn to_string(&self) -> String {
+        match self {
+            Token::Arrow => format!("->"),
+            Token::Ident(ident) => format!("{}", ident),
+            Token::LParen => format!("("),
+            Token::RParen => format!(")"),
+            Token::Fn => format!("fn"),
+            Token::Float(f) => format!("{}", f),
+            Token::Int(i) => format!("{}", i),
+            Token::LBrace => format!("{{"),
+            Token::RBrace => format!("}}"),
+            Token::SemiColon => format!(";"),
+            Token::Return => format!("return"),
+            Token::Plus => format!("+"),
+            Token::Minus => format!("-"),
+            Token::Star => format!("*"),
+            Token::Slash => format!("/"),
+            Token::LineComment(comment) => format!("{}", comment),
+            Token::Modulo => format!("%"),
+            Token::StringLiteral(s) => format!("\"{}\"", s),
+            Token::Let => format!("let"),
+            Token::Const => format!("const"),
+            Token::Comma => format!(","),
+            Token::LeftAngleBracket => format!("<"),
+            Token::RightAngleBracket => format!(">"),
+            Token::Equal => format!("="),
+            Token::EqualEqual => format!("=="),
+            Token::GreaterEqual => format!(">="),
+            Token::LesserEqual => format!("<="),
+            Token::BitAnd => format!("&"),
+            Token::BitOr => format!("|"),
+            Token::Or => format!("||"),
+            Token::And => format!("&&"),
+            Token::Colon => format!(":"),
+            Token::QuestionMark => format!("?"),
+            Token::DoubleColon => format!("::"),
+            Token::Not => format!("!"),
+            Token::NotEqual => format!("!="),
+            Token::Dot => format!("."),
+            Token::For => format!("for"),
+            Token::PlusPlus => format!("++"),
+            Token::MinusMinus => format!("--"),
+            Token::MultiLineComment(comment) => format!("{}", comment),
+            Token::Impl => format!("impl"),
+        }
+    }
+}
+
+pub struct SpannedToken {
+    pub token: Token,
+    pub span: Range<usize>,
+}
+
+pub struct Lexer {
+    pub tokens: Vec<SpannedToken>,
+    pub errors: Vec<String>,
+}
+
+impl Lexer {
+    pub fn new(source: String) -> Lexer {
+        let mut lex = Token::lexer(&source);
+
+        let mut tokens = Vec::new();
+
+        let mut errors = Vec::<String>::new();
+
+        while let Some(result) = lex.next() {
+            let span = lex.span();
+            match result {
+                Ok(token) => tokens.push(SpannedToken { token, span }),
+                Err(_) => errors.push(format!(
+                    "Lexing error at {:?} '{}'",
+                    lex.span(),
+                    &source[span.clone()]
+                )),
+            }
+        }
+
+        Lexer { tokens, errors }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use logos::Logos;
-
-    use crate::lexer::Token;
+    use crate::lexer::{Lexer, Token};
 
     #[test]
     pub fn lex_example_programs() {
         let sources = [
             "
 for(let i = 0; i <=5 ; i++) {
-    let x = i * i;
+    let x = i * i; 
 }
 
 
@@ -144,6 +241,7 @@ for(let i = 0; i <=5 ; ++i) {
         ",
             "
 fn main(x: i32, y: i32) -> i32 {
+    let x = \"String example\";
     return x * 2 * y;
 }
 
@@ -167,12 +265,9 @@ fn main() -> void {
         ",
         ];
         for source in sources {
-            let mut lexer = Token::lexer(source);
+            let lexer = Lexer::new(source.to_string());
 
-            for token in lexer.by_ref() {
-                println!("Token : {:?}", token);
-                assert!(token.is_ok());
-            }
+            assert!(lexer.errors.is_empty());
         }
     }
 }
